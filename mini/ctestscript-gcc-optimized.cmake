13a7de5085f93d8d61555da8f80504da7d6f8bd4
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2014-02-05 12:48:52 +0100

    added mini test configuration

---
+++ mini/ctestscript-gcc-optimized.cmake
@@ -0,0 +1,17 @@
+
+set(ROOT_DIRECTORY "/Users/vdev/git/ViennaMath")
+set(CTEST_SOURCE_DIRECTORY "${ROOT_DIRECTORY}/viennamath-dev")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_BUILD_OPTIONS "")
+SET(ENV{CC} gcc)
+SET(ENV{CXX} g++)
+set(COMPILER_NAME "GCC")
+set(COMPILER_VERSION "4.2.1")
+#set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
+#set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
+set(WITH_MEMCHECK FALSE)
+set(WITH_COVERAGE FALSE)
+
+########################################################################
+include(../ctestscript-common.cmake)
