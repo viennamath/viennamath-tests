d0f9b8d88accfd45f27af7f0f9af93ba141a2a0f
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2014-02-05 13:18:02 +0100

    building and running mini tests in parallel now

--- mini/ctestscript-gcc-debug.cmake
+++ mini/ctestscript-gcc-debug.cmake
@@ -8,8 +8,8 @@ SET(ENV{CC} gcc)
 SET(ENV{CXX} g++)
 set(COMPILER_NAME "GCC")
 set(COMPILER_VERSION "4.2.1")
-#set(CTEST_BUILD_FLAGS -j4) # build tests in parallel
-#set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4") # run tests in parallel
+set(CTEST_BUILD_FLAGS -j2) # build tests in parallel
+set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j2") # run tests in parallel
 set(WITH_MEMCHECK FALSE)
 set(WITH_COVERAGE FALSE)
 
